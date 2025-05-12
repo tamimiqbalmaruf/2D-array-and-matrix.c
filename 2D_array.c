786a5482f7f823dfc26cb8918677870613a3d30b
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-12 14:18:51 +0600

    2D array input and output

--- 2D_array.c
+++ 2D_array.c
@@ -2,8 +2,29 @@
 
 int main()
 {
-    int a[3][4];
+    // int a[3][4];
 
+    // a[1][2] = 10;
+
+    // printf("%d", a[1][2]);
+
+    int r, c;
+
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    
 
     return 0;
 };
