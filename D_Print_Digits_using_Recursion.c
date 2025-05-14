0767c1017845ed6fb19542dc39e7869a493e90df
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-14 16:09:47 +0600

    Print digits using recursion

---
+++ D_Print_Digits_using_Recursion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void rec(int n)
+{
+
+    if (n == 0)
+    {
+        return;
+    }
+
+    rec(n / 10);
+    int lastDigit = n % 10;
+    printf("%d ", lastDigit);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int value;
+        scanf("%d", &value);
+        rec(value);
+        printf("\n");
+    }
+
+    return 0;
+};
