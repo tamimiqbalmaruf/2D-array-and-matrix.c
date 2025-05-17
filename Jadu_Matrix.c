976e17a3ef7df3ae0e3c308b711316fcafeed8cc
Tamim Iqbal Maruf <marufmd775@gmail.com>
2025-05-18 05:20:47 +0600

    Jadu Matrix

--- Jadu_Matrix.c
+++ Jadu_Matrix.c
@@ -11,7 +11,7 @@ int main()
     {
         for (int j = 0; j < c; j++)
         {
-            scanf("%d", &a[i][c]);
+            scanf("%d", &a[i][j]);
         }
     }
 
@@ -26,37 +26,25 @@ int main()
 
                 if (i == j && a[i][j] != 1)
                 {
-                    printf("hello1 => %d %d \n", i,j);
                     jaduMatrix = 0;
                 }
                 else if (i + j == r - 1 && a[i][j] != 1)
                 {
-                    printf("hello2 => %d %d \n", i,j);
                     jaduMatrix = 0;
                 }
 
-                else if (i != j && i + j != r - 1 && a[i][j] != 0)
+                if (i != j && i + j != r - 1 && a[i][j] != 0)
                 {
-                    printf("hello3 => %d %d \n", i,j);
                     jaduMatrix = 0;
                 }
             }
         };
     }
+    else
+    {
+        jaduMatrix = 0;
+    }
 
-    // for (int i = 0; i < r; i++)
-    // {
-    //     for (int j = 0; j < c; j++)
-    //     {
-
-    //         if (i + j != r - 1 && a[i][j] != 0)
-    //         {
-    //             jaduMatrix = 0;
-    //         }
-    //     }
-    // };
-
-    // printf("%d", jaduMatrix);
     printf("%s", jaduMatrix ? "YES" : "NO");
 
     return 0;
